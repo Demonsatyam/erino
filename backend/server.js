dbfd4063fb4d49880dd1481664c8054a35347f35
Demonsatyam <kumarsatyam01303@gmail.com>
2025-08-23 13:22:44 +0530

    Initialize server with MongoDB connection and environment setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-const app = require('./app');
 const dotenv = require('dotenv');
+dotenv.config();
+
+const app = require('./app');
 const connectDB = require('./config/db');
 
-dotenv.config();
+
 
 // Connect to DB
 connectDB();
